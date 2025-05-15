c4bb0c835d6cf7192010f912fc4c321e88386399
geirstorvik <geirs@uio.no>
2025-05-15 14:32:32 +0200

    First push

---
+++ R/SMC_SIR.R
@@ -0,0 +1,19 @@
+---
+title: "SIR Trento"
+output: pdf_notebook
+---
+
+Notebook for performing sequential Monte Carlo on SIR model 
+
+```{r data, echo=FALSE}
+library(data.table)
+library(ggplot2)
+x = read.csv("../data/data_covid19_lab_by_time_latest.csv")
+x = 
+```
+
+Add a new chunk by clicking the *Insert Chunk* button on the toolbar or by pressing *Ctrl+Alt+I*.
+
+When you save the notebook, an HTML file containing the code and output will be saved alongside it (click the *Preview* button or press *Ctrl+Shift+K* to preview the HTML file).
+
+The preview shows you a rendered HTML copy of the contents of the editor. Consequently, unlike *Knit*, *Preview* does not run any R code chunks. Instead, the output of the chunk when it was last run in the editor is displayed.
